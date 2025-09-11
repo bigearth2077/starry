599b0205070a932a5af3b148b6b7df0196875edf
bigearth <2935630783@qq.com>
2025-09-11 21:35:00 +0800

    ignore eslint while building

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // 生产构建时忽略 ESLint（代码仍会在本地或CI跑 lint）
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
